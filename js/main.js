69ed1992f21b8fc92e1eff709020a492976f8e94
jchu4483 <jchu4483@gmail.com>
2018-07-21 16:24:52 -0400

    Confirmed that fetch requests are returning data and able to out pet names to page

--- js/main.js
+++ js/main.js
@@ -1,18 +1,31 @@
 import fetchJsonp from 'fetch-jsonp';
 
-const apiKey = '97e3f6aacd9490ea9406074d72195328';
+const apiKey = '';
 const petForm = document.querySelector('#petForm');
 petForm.addEventListener('submit', handleForm);
 
 function handleForm(e) {
   e.preventDefault();
   const petName = document.querySelector('#petName').value
-  
-  fetchJsonp(`http://api.petfinder.com/pet.find?format=json&key=${apiKey}&animal=${petName}&callback=callback`, 
+  const zipcode = document.querySelector('#zipcode').value
+  console.log(zipcode)
+  fetchJsonp(`http://api.petfinder.com/pet.find?format=json&key=${apiKey}&animal=${petName}&location=${zipcode}&callback=callback`, 
     {
       jsonpCallbackFunction: 'callback'
     }
   )
   .then(res => res.json())
-  .then(data => console.log(data))
+  .then(data => handlePetResponse(data.petfinder.pets.pet))
+  .catch(err => console.log(err))
+}
+
+function handlePetResponse(pets) {
+  const petListings = document.querySelector('#petsListings');
+  console.log(petListings)
+  const petsHTML = pets.map(pet => {
+    return `
+      <li>${pet.name.$t}</li>
+    `
+  }).join('')
+  petListings.innerHTML = petsHTML
 }
